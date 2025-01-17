3b7f03d2dc623b0d906da73974d883ed9619a228
ChyunKim <chkim@realsn.com>
2025-01-17 15:44:46 +0900

    mod: roution label 수정

--- src/widgets/home/ui/HomeMenu.tsx
+++ src/widgets/home/ui/HomeMenu.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 
 export default function HomeMenu() {
   return (
-    <div className="bg-gray-50 px-4 py-6 flex items-center justify-center gap-5 rounded-md border border-gray-100">
+    <div className="bg-gray-50 px-3 py-6 flex items-center justify-center gap-6 rounded-md border border-gray-100">
       <RoutingButton type="both" src="/next.svg" href="/" label="추천 맛집" />
       <RoutingButton type="both" src="/next.svg" href="/" label="추천 카페" />
       <RoutingButton type="both" src="/next.svg" href="/" label="추천 술집" />
-      <RoutingButton type="both" src="/next.svg" href="/" label="종류별 검색" />
-      <RoutingButton type="both" src="/next.svg" href="/" label="지역별 검색" />
+      <RoutingButton type="both" src="/next.svg" href="/" label="애견 동반" />
+      <RoutingButton type="both" src="/next.svg" href="/" label="종류별" />
     </div>
   );
 }
