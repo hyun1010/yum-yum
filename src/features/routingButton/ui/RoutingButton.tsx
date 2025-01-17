6219933dbd8f21580832bae69534158eb54156a2
ChyunKim <chkim@realsn.com>
2025-01-17 16:15:44 +0900

    mod: header roution button 컴포넌트 사용

--- src/features/routingButton/ui/RoutingButton.tsx
+++ src/features/routingButton/ui/RoutingButton.tsx
@@ -2,6 +2,7 @@
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import React from 'react';
+import { ClassNameValue, twMerge } from 'tailwind-merge';
 
 interface RoutingButtogPropsBase {
   href: string; // 항상 필수
@@ -10,19 +11,25 @@ interface RoutingButtogPropsBase {
 interface RoutingButtogPropsLabel extends RoutingButtogPropsBase {
   type: 'onlyLabel'; // 'onlyLabel'일 때는 label만 필수
   label: string;
+  labelStyle?: ClassNameValue; // Tailwind 스타일을 위한 옵션 추가
   src?: never; // 'onlyLabel'일 땐 src는 절대 들어오지 않음
+  iconStyle?: never;
 }
 
 interface RoutingButtogPropsIcon extends RoutingButtogPropsBase {
   type: 'onlyIcon'; // 'onlyIcon'일 때는 src만 필수
   label?: never; // 'onlyIcon'일 땐 label은 절대 들어오지 않음
+  labelStyle?: never;
   src: string;
+  iconStyle?: ClassNameValue;
 }
 
 interface RoutingButtogPropsBoth extends RoutingButtogPropsBase {
   type: 'both'; // 'both'일 때는 label과 src 모두 필수
   label: string;
+  labelStyle?: ClassNameValue;
   src: string;
+  iconStyle?: ClassNameValue;
 }
 
 // 타입이 'onlyLabel', 'onlyIcon', 'both'에 따라 props 타입을 다르게 설정
@@ -36,6 +43,8 @@ export default function RoutingButton({
   href,
   label,
   src,
+  labelStyle,
+  iconStyle,
 }: RoutingButtogProps) {
   const router = useRouter();
 
@@ -49,7 +58,12 @@ export default function RoutingButton({
       onClick={handleClick}
     >
       {type !== 'onlyLabel' && (
-        <div className="relative h-16 w-16 rounded-lg bg-gray-100 border border-gray-50">
+        <div
+          className={twMerge(
+            'relative h-16 w-16 rounded-lg bg-gray-100 border border-gray-50',
+            iconStyle
+          )}
+        >
           <Image
             fill
             src={src}
@@ -59,7 +73,11 @@ export default function RoutingButton({
         </div>
       )}
       {type !== 'onlyIcon' && (
-        <div className="text-sm text-gray-900 text-center">{label}</div>
+        <div
+          className={twMerge('text-sm text-gray-900 text-center', labelStyle)} // labelStyle 적용
+        >
+          {label}
+        </div>
       )}
     </div>
   );
--- src/widgets/layout/ui/Header.tsx
+++ src/widgets/layout/ui/Header.tsx
@@ -1,3 +1,5 @@
+import { RoutingButton } from '@/src/features/routingButton';
+
 export default function Header() {
   return (
     <header className="sticky top-0 z-10 bg-white border-b border-gray-100">
@@ -6,10 +8,20 @@ export default function Header() {
         <nav>
           <ul className="flex space-x-4">
             <li className="text-md text-gray-600 hover:text-gray-900 cursor-pointer">
-              마이페이지
+              <RoutingButton
+                type="onlyLabel"
+                label="마이페이지"
+                href="/"
+                labelStyle="text-md"
+              />
             </li>
             <li className="flex flex-col gap-1 text-md text-gray-600 hover:text-gray-900 cursor-pointer">
-              로그인
+              <RoutingButton
+                type="onlyLabel"
+                label="로그인"
+                href="/"
+                labelStyle="text-md"
+              />
             </li>
           </ul>
         </nav>
