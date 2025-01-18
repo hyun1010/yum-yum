befd74516ad79401316920258749ea7568402b07
hyun1010 <rcohyun1010@gmail.com>
2025-01-18 18:52:22 +0900

    mod: menu 아이콘 추가

--- src/widgets/home/ui/HomeMenu.tsx
+++ src/widgets/home/ui/HomeMenu.tsx
@@ -1,14 +1,13 @@
 import { RoutingButton } from '@/src/features/routingButton';
-import React from 'react';
 
 export default function HomeMenu() {
   return (
     <div className="bg-gray-50 px-3 py-6 flex items-center justify-center gap-6 rounded-md border border-gray-100">
       <RoutingButton type="both" src="/heart.png" href="/" label="추천 맛집" />
-      <RoutingButton type="both" src="/heart.png" href="/" label="추천 카페" />
-      <RoutingButton type="both" src="/heart.png" href="/" label="추천 술집" />
-      <RoutingButton type="both" src="/heart.png" href="/" label="애견 동반" />
-      <RoutingButton type="both" src="/heart.png" href="/" label="종류별" />
+      <RoutingButton type="both" src="/cake.png" href="/" label="추천 카페" />
+      <RoutingButton type="both" src="/wine.png" href="/" label="추천 술집" />
+      <RoutingButton type="both" src="/pets.png" href="/" label="애견 동반" />
+      <RoutingButton type="both" src="/colors.png" href="/" label="종류별" />
     </div>
   );
 }
