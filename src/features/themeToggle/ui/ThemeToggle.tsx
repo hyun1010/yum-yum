fda096a128c830a31d9ab3b0fbbe4b601f7e7910
hyun1010 <rcohyun1010@gmail.com>
2025-02-20 21:41:36 +0900

    feat: hydration error 안나도록 수정

--- src/features/themeToggle/ui/ThemeToggle.tsx
+++ src/features/themeToggle/ui/ThemeToggle.tsx
@@ -1,13 +1,20 @@
 'use client';
 import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 import { CiDark, CiLight } from 'react-icons/ci';
 
 export default function ThemeToggler() {
   const { theme, setTheme } = useTheme();
+  const [initialTheme, setInitialTheme] = useState<'dark' | 'light'>('dark');
 
   const handleToggle = () => {
     setTheme(theme === 'dark' ? 'light' : 'dark');
   };
+
+  useEffect(() => {
+    const userTheme = (theme as 'dark' | 'light') || 'dark';
+    setInitialTheme(userTheme);
+  }, [theme]);
 
   return (
     <button
@@ -15,7 +22,7 @@ export default function ThemeToggler() {
       onClick={handleToggle}
       className="flex items-center justify-center text-black dark:text-white rounded-full cursor-pointer bg-gray-2"
     >
-      {theme === 'dark' ? <CiLight size={30} /> : <CiDark size={30} />}
+      {initialTheme === 'dark' ? <CiLight size={30} /> : <CiDark size={30} />}
     </button>
   );
 }
