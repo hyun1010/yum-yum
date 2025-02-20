a6eed72f7a5a32081548340ae308054626614352
ChyunKim <chkim@realsn.com>
2025-02-20 16:30:55 +0900

    mod: 컴포넌트 분리

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,27 @@
-import { HomeMenu } from '../widgets/home';
-import { Map } from '../widgets/map';
+import AboutSectionOne from '@/components/About/AboutSectionOne';
+import AboutSectionTwo from '@/components/About/AboutSectionTwo';
+import Blog from '@/components/Blog';
+import Brands from '@/components/Brands';
+import Contact from '@/components/Contact';
+import Features from '@/components/Features';
+import Pricing from '@/components/Pricing';
+import Testimonials from '@/components/Testimonials';
+import Video from '@/components/Video';
+import ShowCase from '@/src/widgets/showcase/ui/ShowCase';
 
 export default function HomePage() {
   return (
     <>
-      <HomeMenu />
-      <Map />
+      <ShowCase />
+      <Features />
+      <Video />
+      <Brands />
+      <AboutSectionOne />
+      <AboutSectionTwo />
+      <Testimonials />
+      <Pricing />
+      <Blog />
+      <Contact />
     </>
   );
 }
