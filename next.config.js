a3639000f9658c0d237d12e3784e6d5cd4f5c1d7
ChyunKim <chkim@realsn.com>
2025-02-21 10:27:13 +0900

    feat: external image url set

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'tong.visitkorea.or.kr',
+        port: '',
+        pathname: '/cms2/website/**',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
